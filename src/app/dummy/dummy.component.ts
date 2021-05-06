de4d65e8971ce3635ec965951f463087b3c61db9
Ashok0710 <66946002+Ashok0710@users.noreply.github.com>
2021-05-07 01:59:14 +0530

    [feat] home page

--- src/app/dummy/dummy.component.ts
+++ src/app/dummy/dummy.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DummyComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('Dummy component');
+   }
 
   ngOnInit(): void {
   }
--- src/app/sidenavbar/sidenavbar.component.ts
+++ src/app/sidenavbar/sidenavbar.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class SidenavbarComponent implements OnInit {
 
   constructor(private router:Router, private activated:ActivatedRoute) { 
-    
+    console.log('Sidenavbar component');
   }
 
   ngOnInit(): void {
